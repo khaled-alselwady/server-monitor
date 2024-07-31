caf2a2ae3ae4d638bcc351e9624c98cf9f74dc9b
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-01 01:43:24 +0300

    feat(new-ticket): add new-ticket component

---
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-ticket',
+  standalone: true,
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css',
+})
+export class NewTicketComponent {}
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [NewTicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
