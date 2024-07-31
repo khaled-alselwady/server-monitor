00d8038abc6d8aae4bdbb214f09bf17be2898883
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-01 01:15:29 +0300

    feat(dashboard/dashboard-item): use content-projection with ng-content in the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ import { TicketsComponent } from './dashboard/tickets/tickets.component';
     ServerStatusComponent,
     TrafficComponent,
     TicketsComponent,
-  ],
+    DashboardItemComponent
+],
 })
 export class AppComponent {}
--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
