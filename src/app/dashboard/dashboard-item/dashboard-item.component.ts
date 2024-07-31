c4d768e73ba4a5e1c6dfac0364a670e564f55751
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-01 00:10:58 +0300

    feat(dashboard/dashboard-item): add dashboard-item component

---
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css',
+})
+export class DashboardItemComponent {
+  // @Input({ required: true }) image!: { src: string; alt: string };
+  // @Input({ required: true }) title!: string;
+
+  image = input.required<{ src: string; alt: string }>();
+  title = input.required<string>();
+}
