8f1d98a15da44190c932caa8edf0dd5852253497
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-01 03:06:07 +0300

    feat(shared/button): extend built-in button element with Custom Components via Attribute Selectors

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'button[appButton]', // Attribute selector => for access any button that has an attribute (appButton)
+  // selector: 'button.app-button', // Class selector => for access any button that has a class (app-button)
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+}
