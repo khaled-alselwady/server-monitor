c3fa116a2ebfa957aa8bd5624b0daf4f7b392536
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-02 23:49:26 +0300

    feat(shared/control): access host element programmatically through dependency injection using the 'ElementRef' class

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,7 +1,9 @@
 import {
   Component,
+  ElementRef,
   HostBinding,
   HostListener,
+  inject,
   input,
   ViewEncapsulation,
 } from '@angular/core';
@@ -26,7 +28,10 @@ export class ControlComponent {
   // }
   // we preferred to use host element that inside the component instead of using @HostBinding & @HostListener
 
+  private el = inject(ElementRef);
+
   onClick() {
     console.log('Control clicked!');
+    console.log(this.el); // using that to interact with DOM of the host element 'app-control' directly
   }
 }
