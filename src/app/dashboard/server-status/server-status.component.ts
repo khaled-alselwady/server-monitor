45b402c8821380453c217b50010572437eeb2da0
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-03 07:03:26 +0300

    feat(dashboard/server-status): component cleanup with ngOnDestroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,13 +7,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private intervalId?: ReturnType<typeof setInterval>;
 
   constructor() {}
 
   ngOnInit() {
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
       const rnd = Math.random(); // 0 - 0.99999999999999...
 
       if (rnd < 0.5) {
@@ -24,5 +25,9 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
   }
 }
