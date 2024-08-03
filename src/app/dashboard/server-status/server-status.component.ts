4fbbc07d143dd624fce19a974e86c30eea8ea85f
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-03 06:08:20 +0300

    feat(dashboard/server-status): use ngOnInit method instead of constructor to update the component's state based on its initial input values

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -10,7 +10,9 @@ import { Component } from '@angular/core';
 export class ServerStatusComponent {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {
     setInterval(() => {
       const rnd = Math.random(); // 0 - 0.99999999999999...
 
