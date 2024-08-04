25ac23ac437af71570931936f36b726ec1badb8a
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-04 07:42:34 +0300

    feat(new-ticket): use the viewChild signal function instead of decorator

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,10 +11,11 @@ import { FormsModule } from '@angular/forms';
   imports: [ButtonComponent, ControlComponent, FormsModule],
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
   onSubmit(title: string, ticketText: string) {
     console.log(title);
     console.log(ticketText);
-    this.form?.nativeElement.reset(); // Clear form inputs after submission
+    this.form().nativeElement.reset(); // Clear form inputs after submission
   }
 }
