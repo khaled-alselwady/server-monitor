a7c7872740ed9fede4ddb22624cdccef7da978d0
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-07 01:44:56 +0300

    feat(ticket): add ticket.model interface

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
-
+  Tickets: Ticket[] = [];
 }
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
-export class TicketsComponent {}
+export class TicketsComponent {
+  
+}
