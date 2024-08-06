6786412d8ba71fa4a908b82ae26456820860c556
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-07 01:42:01 +0300

    feat(ticket): add ticket component

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [],
   templateUrl: './tickets.component.html',
-  styleUrl: './tickets.component.css'
+  styleUrl: './tickets.component.css',
 })
-export class TicketsComponent {
-
-}
+export class TicketsComponent {}
